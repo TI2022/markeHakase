ab5a16624f02242b0c41bd02e1fd356792c43510
Yoshi3lines <ny3line@gmail.com>
2022-01-30 22:17:44 +0900

    fix/devise_PR01

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -178,7 +178,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..40
+  config.password_length = 6..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
@@ -224,7 +224,6 @@ Devise.setup do |config|
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
   # change their passwords.
-  # パスワードリセットしてから再設定可能な有効期限。
   config.reset_password_within = 24.hours
 
   # When set to false, does not sign a user in automatically after their password is
